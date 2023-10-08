f7823cc92bd8c1d34348a7469034a50cc0762c48
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-10-08 22:22:05 +0800

    more box styling

--- ui/src/components/Box.tsx
+++ ui/src/components/Box.tsx
@@ -6,7 +6,10 @@ interface BoxProps {
 
 const Box: React.FC<BoxProps> = ({ children }) => {
   return (
-    <div className='relative bg-black border border-gray-600 rounded-2xl h-[550px] w-[800px] flex items-center justify-center hover:shadow-xl'>
+    <div
+      className='relative bg-black border border-gray-600 rounded-3xl h-[550px] w-[800px] flex items-center justify-center hover:shadow-xl transition-shadow duration-500 ease-in-out'
+      style={{ willChange: 'box-shadow' }}
+    >
       {children}
     </div>
   )  
--- ui/src/providers/KoinuBaseGoerli.tsx
+++ ui/src/providers/KoinuBaseGoerli.tsx
@@ -7,6 +7,7 @@ const KoinuBaseGoerli = () => {
       <div className="absolute left-0 top-0 bottom-0 w-1/3 flex flex-col items-start justify-start p-6">
         <Title />
       </div>
+      
       <div className="absolute top-0 bottom-0 w-px border border-gray-700 left-1/3"></div>
     </Box>
   )
