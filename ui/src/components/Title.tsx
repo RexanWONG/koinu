07e7ff7c4393f167565501b33d0d78f2134103b9
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-10-09 21:49:55 +0800

    update title styles

--- ui/src/components/Title.tsx
+++ ui/src/components/Title.tsx
@@ -1,7 +1,7 @@
 const Title = () => {
     return (
-      <h4 className="text-white scroll-m-20 text-xl font-semibold tracking-tight">
-        🐕 <span  className="ml-1"/> Koinu
+      <h4 className="text-white scroll-m-20 text-xl font-extrabold">
+        🐕 <span className="ml-1"/> Koinu
       </h4>
     )
   }
