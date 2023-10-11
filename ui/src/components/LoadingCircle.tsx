7fa210cd971ab6caeb1f8c6a72311257bd4e016c
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-10-11 21:52:48 +0800

    display gas

--- ui/src/components/LoadingCircle.tsx
+++ ui/src/components/LoadingCircle.tsx
@@ -1,8 +1,6 @@
 const LoadingCircle = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-48 h-48 border-t-4 border-white border-solid rounded-full animate-spin"></div>
-    </div>
+    <div className="w-48 h-48 border-t-4 border-white border-solid rounded-full animate-spin" />
   );
 };
 
--- ui/src/providers/KoinuBaseGoerli.tsx
+++ ui/src/providers/KoinuBaseGoerli.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useState, ChangeEvent } from 'react';
 import { Network, Alchemy } from 'alchemy-sdk';
 import { ethers } from 'ethers';
+import { 
+  AxelarQueryAPI,
+  Environment,
+  EvmChain,
+  GasToken
+} from "@axelar-network/axelarjs-sdk";
+
 import { formatBalance } from '../utils';
 
 import Box from '../components/Box'
@@ -15,6 +22,7 @@ import TransactionStatistics from '../components/TransactionStatistics';
 
 import { baseGoerliDeployedAddress, opGoerliDeployedAddress } from '../constants';
 import koinuBaseGoerliAbi from '../constants/KoinuBaseGoerli.json'
+import LoadingCircle from '../components/LoadingCircle';
 
 interface KoinuBaseGoerliProps {
   signer: any;
@@ -26,12 +34,15 @@ const KoinuBaseGoerli: React.FC<KoinuBaseGoerliProps> = ({ signer }) => {
   const [connectedAddress, setConnectedAddress] = useState('')
   const [baseGoerliBalance, setBaseGoerliBalance] = useState(0)
   const [optimismGoerliBalance, setOptimismGoerliBalance] = useState(0);
+  const [gasFee, setGasFee] = useState(0)
   const [inputValue, setInputValue] = useState({
     optimismGoerliBridgeAmount: Number(0)
   });
 
   const [isAllDataLoaded, setIsAllDataLoaded] = useState(false)
   const [isOptimismGoerliSelected, setIsOptimismGoerliSelected] = useState(false);
+  const [isSendToDifferentChainLoading, setIsSendToDifferentChainLoading] = useState(false)
+  const [isSentToDifferentChain, setIsSentToDifferentChain] = useState(false)
   
   const OP_GOERLI_API_KEY = import.meta.env.VITE_OP_GOERLI_API_KEY;
   const BASE_GOERLI_API_KEY = import.meta.env.VITE_BASE_GOERLI_API_KEY;
@@ -39,11 +50,17 @@ const KoinuBaseGoerli: React.FC<KoinuBaseGoerliProps> = ({ signer }) => {
   const alchemyOpGoerliProvider = new Alchemy({ apiKey: OP_GOERLI_API_KEY, network: Network.OPT_GOERLI });
   const alchemyBaseGoerliProvider = new Alchemy({ apiKey: BASE_GOERLI_API_KEY, network: Network.BASE_GOERLI });
 
+  const axelarSdk = new AxelarQueryAPI({
+    environment: Environment.TESTNET,
+  });
+
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue((prevFormData) => ({
       ...prevFormData,
       [event.target.name]: event.target.value,
-    }));    
+    }));   
+    
+    getEstimatedGasOpGoerli();
   };
 
   const fetchAddress = async () => {
@@ -73,24 +90,58 @@ const KoinuBaseGoerli: React.FC<KoinuBaseGoerliProps> = ({ signer }) => {
     }
   };
 
+  const getEstimatedGasOpGoerli = async () => {
+    try {
+      const gmpParams = {
+        showDetailedFees: true,
+        transferAmount: inputValue.optimismGoerliBridgeAmount, 
+        destinationContractAddress: opGoerliDeployedAddress,
+        sourceContractAddress: baseGoerliDeployedAddress,
+        tokenSymbol: "aUSDC"
+      };
+  
+      const gas = await axelarSdk.estimateGasFee(
+        EvmChain.BASE,
+        EvmChain.OPTIMISM,
+        GasToken.ETH,
+        700000,
+        2,
+        '10',
+        gmpParams
+      );
+  
+      if (typeof gas !== 'string' && gas.baseFee) {
+        setGasFee(Number(ethers.utils.formatUnits(gas.baseFee)));
+      } 
+    } catch (error) {
+      console.error('Failed to fetch the gas fee');
+    }
+  };
+
   const handleSendToDifferentChain = async (chainName: string) => {
     try {
       if (chainName === 'optimism') {
+        setIsSendToDifferentChainLoading(true)
         const send = await koinuBaseGoerliConntract.sendToDifferentChain(
           chainName,
           opGoerliDeployedAddress,
           'aUSDC',
           inputValue.optimismGoerliBridgeAmount,
           {
-            value: ethers.utils.parseEther('0.08')
+            value: ethers.utils.parseEther(String(gasFee))
           }
         )
 
+        console.log(send)
         await send.wait()
+
+        setIsSendToDifferentChainLoading(false)
+        setIsSentToDifferentChain(true)
         console.log(send)
       }
     
     } catch (error) {
+      setIsSendToDifferentChainLoading(false)
       alert(error)
     }
   }
@@ -100,6 +151,7 @@ const KoinuBaseGoerli: React.FC<KoinuBaseGoerliProps> = ({ signer }) => {
         await fetchAddress();
         await fetchBalance(alchemyOpGoerliProvider, setOptimismGoerliBalance, connectedAddress);
         await fetchBalance(alchemyBaseGoerliProvider, setBaseGoerliBalance, connectedAddress);
+        await getEstimatedGasOpGoerli()
 
         setTimeout(() => {
           setIsAllDataLoaded(true);
@@ -107,6 +159,10 @@ const KoinuBaseGoerli: React.FC<KoinuBaseGoerliProps> = ({ signer }) => {
     }
     fetchData();
   }, [isAllDataLoaded, setIsAllDataLoaded, connectedAddress, setConnectedAddress, signer]);
+
+  useEffect(() => {
+    getEstimatedGasOpGoerli();
+  }, [inputValue.optimismGoerliBridgeAmount]);
   
   return (
     <Box isAllDataLoaded={isAllDataLoaded}>
@@ -128,66 +184,81 @@ const KoinuBaseGoerli: React.FC<KoinuBaseGoerliProps> = ({ signer }) => {
 
       <div className="absolute left-1/3 top-0 bottom-0 w-2/3 flex flex-col items-center justify-center">
         {isOptimismGoerliSelected? (
-          <div className='flex flex-col items-center justify-center'>
-            <ActionBar 
-              chainName={'Optimism Goerli'}
-              action1={() => setIsOptimismGoerliSelected(false)}
-              action2={() => {}}
-            />
-
-            <div className='flex flex-col items-center justify-center mt-16'>
-              <div className='bg-gray-900 h-[80px] w-[400px] rounded-lg flex items-center justify-start transform hover:scale-105 duration-1000'>
-                <div className='flex flex-col ml-5'>
-                  <p className="text-gray-400 text-sm">Amount</p>
-                    <input 
-                      name='optimismGoerliBridgeAmount'
-                      type='number'
-                      placeholder="0"
-                      className="bg-transparent border-none text-white text-2xl placeholder-gray-400 w-none outline-none"
-                      onChange={handleInputChange}
-                      value={inputValue.optimismGoerliBridgeAmount}
-                    /> 
-                </div>
-                <img 
-                  src={Optimism}
-                  alt='Optimism'
-                  className='w-[20px] object-contain ml-10 mr-2'
-                /> 
-                <h4 className="text-right scroll-m-20 text-white text-xl font-semibold tracking-tight">
-                  ETH 
-                </h4>
-              </div>
-              <div className='flex flex-row items-center justify-center text-sm gap-48 mr-4 mt-2'>
-                <p className="text-gray-400">
-                  Balance : {formatBalance(optimismGoerliBalance)} ETH
-                </p>
-                <p onClick={() => setMaxBalance('optimismGoerli')} className='text-red-400 font-extrabold hover:underline hover:cursor-pointer'>
-                  MAX
-                </p>
-              </div>
-
-              <div className='flex flex-col items-cennter justify-center mt-10'>
-                  <TransactionStatistics 
-                    chainImage={Optimism}
+          isSendToDifferentChainLoading ? (
+            <div className='flex flex-col items-center justify-center'>
+              <LoadingCircle />
+              <p className="text-center text-gray-400 mt-10">
+                Sending {inputValue.optimismGoerliBridgeAmount} ETH to Optimism Goerli
+              </p>
+            </div>
+          ) : (
+            isSentToDifferentChain ? (
+              <div />
+            ) : (
+              <div className='flex flex-col items-center justify-center'>
+                  <ActionBar 
                     chainName={'Optimism Goerli'}
-                    originalBalance={Number(formatBalance(optimismGoerliBalance))}
-                    newBalance={Number((Number(formatBalance(optimismGoerliBalance)) - Number(inputValue.optimismGoerliBridgeAmount)).toFixed(3))}
+                    action1={() => setIsOptimismGoerliSelected(false)}
+                    action2={() => {}}
                   />
 
-                  <div className='mt-5'>
-                    <TransactionStatistics 
-                      chainImage={Base}
-                      chainName={'Base Goerli'}
-                      originalBalance={Number(formatBalance(baseGoerliBalance))}
-                      newBalance={Number(Number(formatBalance(baseGoerliBalance) + Number(inputValue.optimismGoerliBridgeAmount)).toFixed(3))}
-                    />
+                  <div className='flex flex-col items-center justify-center mt-16'>
+                    <div className='bg-gray-900 h-[80px] w-[400px] rounded-lg flex items-center justify-start transform hover:scale-105 duration-1000'>
+                      <div className='flex flex-col ml-5'>
+                        <p className="text-gray-400 text-sm">Amount</p>
+                          <input 
+                            name='optimismGoerliBridgeAmount'
+                            type='number'
+                            placeholder="0"
+                            className="bg-transparent border-none text-white text-2xl placeholder-gray-400 w-none outline-none"
+                            onChange={handleInputChange}
+                            value={inputValue.optimismGoerliBridgeAmount}
+                          /> 
+                      </div>
+                      <img 
+                        src={Optimism}
+                        alt='Optimism'
+                        className='w-[20px] object-contain ml-10 mr-2'
+                      /> 
+                      <h4 className="text-right scroll-m-20 text-white text-xl font-semibold tracking-tight">
+                        ETH 
+                      </h4>
+                    </div>
+                    <div className='flex flex-row items-center justify-center text-sm gap-48 mr-4 mt-2'>
+                      <p className="text-gray-400">
+                        Balance : {formatBalance(optimismGoerliBalance)} ETH
+                      </p>
+                      <p onClick={() => setMaxBalance('optimismGoerli')} className='text-red-400 font-extrabold hover:underline hover:cursor-pointer'>
+                        MAX
+                      </p>
+                    </div>
+
+                    <div className='flex flex-col items-cennter justify-center mt-10'>
+                        <TransactionStatistics 
+                          chainImage={Optimism}
+                          chainName={'Optimism Goerli'}
+                          originalBalance={Number(formatBalance(optimismGoerliBalance))}
+                          newBalance={Number((Number(formatBalance(optimismGoerliBalance)) - Number(inputValue.optimismGoerliBridgeAmount)).toFixed(3))}
+                        />
+
+                        <div className='mt-5'>
+                          <TransactionStatistics 
+                            chainImage={Base}
+                            chainName={'Base Goerli'}
+                            originalBalance={Number(formatBalance(baseGoerliBalance))}
+                            newBalance={Number(Number(formatBalance(baseGoerliBalance) + Number(inputValue.optimismGoerliBridgeAmount)).toFixed(3))}
+                          />
+                        </div>
+                    </div>
+
+                    <p className='text-gray-400 text-sm mt-7'>Gas Fee: {gasFee.toFixed(8)} ETH</p>
+                    <button onClick={() => handleSendToDifferentChain('optimism')} className="font-extrabold bg-white hover:bg-gradient-to-l from-white via-orange-500 to-yellow-300 hover:animate-text w-[350px] transition-all duration-150 ease-in-out rounded-xl p-2 mt-7">
+                      Send to OP Goerli!
+                    </button>
                   </div>
-              </div>
-              <button onClick={() => handleSendToDifferentChain('optimism')} className="font-extrabold bg-white hover:bg-gradient-to-l from-white via-orange-500 to-yellow-300 hover:animate-text w-[350px] transition-all duration-150 ease-in-out rounded-xl p-2 mt-10">
-                Send to OP Goerli!
-              </button>
-            </div>
-          </div>
+                </div>
+              )
+            )
         ) : (
           <RightSideDefaultColumn 
             chainImage={Base}
