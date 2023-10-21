9977967d9442c168d8622ecf3cb6cdfdb46ced3e
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-10-22 01:16:23 +0800

    fix deploy error

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'index.ts'),
-      name: 'koinu',
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-    sourcemap: true,
-    emptyOutDir: true
-  },
-  plugins: [react(), dts()],
-});
+  plugins: [react()],
+})
